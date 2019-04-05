6530216e80d01a259a44e0ef0c3e2984bdf72797
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-06 03:10:30 +0900

    リファクタ（一掃）

--- alertscripts/slack.sh
+++ alertscripts/slack.sh
@@ -1,35 +1,45 @@
 #!/bin/bash -x
 
-url='https://hooks.slack.com/services/xxxxxxxxxxxxxxxxx'
-username='Zabbix'
+# Slack incoming web-hook URL
+SLACK_WEBHOOKSURL='https://hooks.slack.com/services/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'
 
-## Values received by this script:
-# To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
-# Subject = $2 (usually either PROBLEM or RECOVERY/OK)
-# Message = $3 (whatever message the Zabbix action sends, preferably something like "Zabbix server is unreachable for 5 minutes - Zabbix server (127.0.0.1)")
+# Slack UserName
+SLACK_USERNAME='Zabbix'
 
-# Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY/OK)
-to="$1"
-subject="$2"
 
-# Change message emoji depending on the subject - smile (RECOVERY/OK), frowning (PROBLEM), or ghost (for everything else)
-recoversub='^RECOVER(Y|ED)?$'
-if [[ "$subject" =~ ${recoversub} ]]; then
-	emoji=':smile:'
-elif [ "$subject" == 'OK' ]; then
-	emoji=':smile:'
-elif [ "$subject" == 'PROBLEM' ]; then
-	emoji=':frowning:'
+# "Send to" for Zabbix User Media Setting
+NOTIFY_CHANNEL="$1"
+
+# "Default subject" for Action Operations Setting
+ALERT_SUBJECT="$2"
+
+# "Default message" for Action Operations Setting
+ALERT_MESSAGE="$3"
+
+if [ "${ALERT_SUBJECT%%:*}" == 'OK' ]; then
+        ICON=':smile:'
+        COLOR="good"
+elif [ "${ALERT_SUBJECT%%:*}" == 'PROBLEM' ]; then
+        ICON=':skull:'
+        COLOR="danger"
 else
-	emoji=':bomb:'
+        ICON=':innocent:'
+        ICON=':sushi:'
+        COLOR="#439FE0"
 fi
 
-echo $emoji
+# Create JSON payload
+PAYLOAD="payload={
+    \"channel\": \"${NOTIFY_CHANNEL//\"/\\\"}\",
+    \"username\": \"${SLACK_USERNAME//\"/\\\"}\",
+    \"icon_emoji\": \"${ICON}\",
+    \"attachments\": [
+        {
+            \"color\": \"${COLOR}\",
+            \"text\": \"${ALERT_MESSAGE//\"/\\\"}\"
+        }
+    ]
+}"
 
-# The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
-#  followed by the message that Zabbix actually sent us ($3)
-message="${subject}: $3"
-
-# Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
-payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
-curl -m 5 --data-urlencode "${payload}" $url -A 'zabbix-slack-alertscript / https://github.com/ericoc/zabbix-slack-alertscript'
+# Send it as a POST request to the Slack incoming webhooks URL
+curl -m 5 --data-urlencode "${PAYLOAD}" $SLACK_WEBHOOKSURL
