a99b1c340bdf0d56c6c017e6a5db683dd679fad1
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-07 16:09:21 +0900

    変数部分修正

--- install_zabbix4_agent_amzn2.sh
+++ install_zabbix4_agent_amzn2.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-Z_Server=20.0.0.167
-Z_Metadata=WEB
+###############################
+Z_Server=xx.xx.xx.xx
+Z_Metadata=XXX_autoregistration
+###############################
 
+# install: agent
 rpm -ivh http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
 yum install zabbix-agent -y
 
+# configure: agent
 cp -p /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.org
-
 sed -i -e "s/^Server=127.0.0.1/Server=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
 sed -i -e "s/^ServerActive=127.0.0.1/ServerActive=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
 sed -i -e "s/^Hostname=Zabbix server/#Hostname=Zabbix server/g" /etc/zabbix/zabbix_agentd.conf
