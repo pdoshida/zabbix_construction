eb851a2dd7e0a31c6388d675d2b2e5aa08f96590
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-07 16:16:42 +0900

    agent conf修正

--- install_zabbix4_agent_amzn2.sh
+++ install_zabbix4_agent_amzn2.sh
@@ -14,7 +14,8 @@ cp -p /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.org
 sed -i -e "s/^Server=127.0.0.1/Server=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
 sed -i -e "s/^ServerActive=127.0.0.1/ServerActive=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
 sed -i -e "s/^Hostname=Zabbix server/#Hostname=Zabbix server/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^# HostMetadata=/# HostMetadata=${Z_Metadata}/g" /etc/zabbix/zabbix_agentd.conf
+sed -i -e "s/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g" /etc/zabbix/zabbix_agentd.conf
+sed -i -e "s/^# HostMetadata=/HostMetadata=${Z_Metadata}/g" /etc/zabbix/zabbix_agentd.conf
 
 systemctl start zabbix-agent
 systemctl enable zabbix-agent
