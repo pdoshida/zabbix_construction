331ea20553aa9cc8678b867aa59d3cced4220fc4
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-26 12:55:20 +0900

    Update install_zabbix4_agent_amzn2.sh

--- install_zabbix4_agent_amzn2.sh
+++ install_zabbix4_agent_amzn2.sh
@@ -7,17 +7,19 @@ Z_Metadata=XXX_autoregistration
 ###############################
 
 # install: agent
-rpm -ivh http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
+rpm -ivh http://repo.zabbix.com/zabbix/4.0/rhel/6/x86_64/zabbix-release-4.0-1.el6.noarch.rpm
 yum install zabbix-agent -y
 
 # configure: agent
-cp -p /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.org
-sed -i -e "s/^Server=127.0.0.1/Server=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^ServerActive=127.0.0.1/ServerActive=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^Hostname=Zabbix server/#Hostname=Zabbix server/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^# HostMetadata=/HostMetadata=${Z_Metadata}/g" /etc/zabbix/zabbix_agentd.conf
+zconf="/etc/zabbix/zabbix_agentd.conf"
+cp -p ${zconf} ${zconf}.org
+sed -i -e "s/^Server=127.0.0.1/Server=${Z_Server}/g" ${zconf}
+sed -i -e "s/^ServerActive=127.0.0.1/ServerActive=${Z_Server}/g" ${zconf}
+sed -i -e "s/^Hostname=Zabbix server/#Hostname=Zabbix server/g" ${zconf}
+sed -i -e "s/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g" ${zconf}
+sed -i -e "s/^# HostMetadata=/HostMetadata=${Z_Metadata}/g" ${zconf}
 
-systemctl start zabbix-agent
-systemctl enable zabbix-agent
-systemctl status zabbix-agent
+chkconfig zabbix-agent on
+chkconfig --list zabbix-agent
+service zabbix-agent start
+service zabbix-agent status
