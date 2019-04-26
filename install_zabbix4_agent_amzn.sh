30b3d67594721c8a69a5b06f4116dd11f3eb4227
taku oshida <40931117+pdoshida@users.noreply.github.com>
2019-04-26 15:04:19 +0900

    Update install_zabbix4_agent_amzn.sh

--- install_zabbix4_agent_amzn.sh
+++ install_zabbix4_agent_amzn.sh
@@ -12,11 +12,16 @@ yum install zabbix-agent -y
 
 # configure: agent
 cp -p /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.org
-sed -i -e "s/^Server=127.0.0.1/Server=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^ServerActive=127.0.0.1/ServerActive=${Z_Server}/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^Hostname=Zabbix server/#Hostname=Zabbix server/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g" /etc/zabbix/zabbix_agentd.conf
-sed -i -e "s/^# HostMetadata=/HostMetadata=${Z_Metadata}/g" /etc/zabbix/zabbix_agentd.conf
+zconf="/etc/zabbix/zabbix_agentd.conf"
+cat /dev/null > ${zconf}
+echo "PidFile=/var/run/zabbix/zabbix_agentd.pid" >> ${zconf}
+echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> ${zconf}
+echo "LogFileSize=0" >> ${zconf}
+echo "Server=${Z_Server}" >> ${zconf}
+echo "ServerActive=${Z_Server}" >> ${zconf}
+echo "Hostname=`uname -n`" >> ${zconf}
+echo "HostMetadata=${Z_Metadata}" >> ${zconf}
+echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> ${zconf}
 
 service zabbix-agent start
 chkconfig zabbix-agent on
